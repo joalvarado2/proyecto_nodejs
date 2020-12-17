6088f3ef9b9e297c2aeaff8748544fd643301344
Jonathan Alvarado <jonathanalva92@gmail.com>
2020-12-16 22:08:42 -0500

    agregando dependencias node

---
+++ server/index.js
@@ -0,0 +1,10 @@
+// importar express
+const express = require("express");
+const routes = require("./routes");
+
+// configurar express
+const app = express();
+
+app.use("/", routes());
+
+app.listen(3000);
